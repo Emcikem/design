475fb9f3a0f9193c11567ca53ca21d066df99725
Emcikem <18968479985@163.com>
2022-08-20 22:48:09 +0800

    init

---
+++ src/threadpool/ExecutorService.java
@@ -0,0 +1,11 @@
+package threadpool;
+
+import java.util.concurrent.Executor;
+
+/**
+ * @author Emcikem
+ * @create 2022/8/20
+ * @desc
+ */
+public interface ExecutorService extends Executor {
+}
---
+++ src/threadpool/ThreadPoolFactory.java
@@ -0,0 +1,9 @@
+package threadpool;
+
+/**
+ * @author Emcikem
+ * @create 2022/8/20
+ * @desc 线程池工厂
+ */
+public class ThreadPoolFactory {
+}
